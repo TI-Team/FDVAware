3b3143cb1e6d4542fd4f7390e1ced1e5235af6f3
Abdias Viana <abdviana@gmail.com>
2020-11-14 12:04:33 -0300

    merge to master

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2020_11_12_160418) do
-=======
 ActiveRecord::Schema.define(version: 2020_11_14_143437) do
->>>>>>> origin/idp
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
